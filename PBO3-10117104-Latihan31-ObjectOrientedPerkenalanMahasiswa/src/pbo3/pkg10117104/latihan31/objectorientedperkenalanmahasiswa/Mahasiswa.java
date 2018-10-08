7f8890b789fd8a33531a719daac2e2d29398bd3d
AdityaSuheryana <43383601+AdityaSuheryana@users.noreply.github.com>
2018-10-09 00:50:36 +0700

    Add files via upload

---
+++ PBO3-10117104-Latihan31-ObjectOrientedPerkenalanMahasiswa/src/pbo3/pkg10117104/latihan31/objectorientedperkenalanmahasiswa/Mahasiswa.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan31.objectorientedperkenalanmahasiswa;
+
+/**
+ *
+ * @author
+ * Nama : Aditya Suheryana
+ * Nim : 10117104
+ * Kelas : IF-3/PBO3
+ * Deskripsi Program : Membuat class baru untuk mendukung program 
+ * perkenalan Mahasiswa
+ */
+public class Mahasiswa {
+
+public String nim;
+public String nama;
+
+public void perkenalkanDiri(String nim,String nama){
+    
+    System.out.println("Hallo Everyone");
+    System.out.println("My NIM is "+nim);
+    System.out.println("My Name is "+nama);
+    System.out.println("");
+    
+}
+    
+}
---
+++ PBO3-10117104-Latihan31-ObjectOrientedPerkenalanMahasiswa/src/pbo3/pkg10117104/latihan31/objectorientedperkenalanmahasiswa/PBO310117104Latihan31ObjectOrientedPerkenalanMahasiswa.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan31.objectorientedperkenalanmahasiswa;
+
+/**
+ *
+ * @author
+ * Nama : Aditya Suheryana
+ * Kelas : IF-3/PBO3
+ * Nim : 10117104
+ * Deskripsi Program : Membuat Program Perkenalan Mahasiswa berbasis Object
+ * 
+ */
+public class PBO310117104Latihan31ObjectOrientedPerkenalanMahasiswa {
+
+   
+    public static void main(String[] args) {
+       Mahasiswa mhs1 = new Mahasiswa();
+       mhs1.perkenalkanDiri("10110269","Rizki Adam Kurniawan");
+       
+       Mahasiswa mhs2 = new Mahasiswa();
+       mhs2.perkenalkanDiri("10110270","Indra Tiola");
+       
+       Mahasiswa mhs3 = new Mahasiswa();
+       mhs3.perkenalkanDiri("10110271","Robi Tansil Ganefi");
+       
+       Mahasiswa mhs4 = new Mahasiswa();
+       mhs4.perkenalkanDiri("10110269","Muhammad Nur Awaludin");
+        
+        
+        
+    }
+    
+}
